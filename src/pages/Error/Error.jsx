1fc44bd8bb0a103bef50fce60f8de9d2dadc2230
mdabarik <mdabarik19@gmail.com>
2023-10-19 16:22:05 +0600

    404 page added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Error = () => {
+
+
     return (
         <div>
-            This is error.jsx
+            <Navbar></Navbar>
+
+            <div className="bg-[#565555] text-white py-14 space-y-5"> 
+                <h2 className="font-bold text-3xl text-center">404! Page Not Found!!</h2>
+                <div className="flex items-center justify-center">
+                <Link className="btn btn-error" to="/">Go To Home</Link>
+                </div>
+            </div>
         </div>
     );
 };
